8a7c40370be65f30cea3fbcdf464498d2449c49c
VillamilA <151254593+VillamilA@users.noreply.github.com>
2024-10-09 09:45:07 -0500

    Add files via upload

---
+++ ComparacionEstricta.js
@@ -0,0 +1,8 @@
+let productId1 = "3451";
+let productId2 = 23461;
+
+if (productId1 === productId2) {
+    console.log("Los IDs son iguales y del mismo tipo.");
+} else {
+    console.log("Los IDs son diferentes o de distinto tipo."); // Se ejecutara esto
+}
